05f51123a832e9128ec23b0ae5ccd95904b08520
haiteng.long <haiteng.long@yintech.cn>
2022-01-20 15:00:02 +0800

    fix:update config

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:KavenLong/snails.git main
+# git push -f git@github.com:KavenLong/snails.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:KavenLong/snails.git main:gh-pages
+git push -f git@github.com:KavenLong/snails.git master:gh-pages
 
 cd -
